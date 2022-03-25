0bb9f83f4c41360e40a6774ae5284279844fccee
Blessing <bigbadumhe@gmail.com>
2022-03-25 12:11:31 -0500

    finished setting up timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ let questionSection = document.querySelector('.questionSection');
 let timeElm = document.getElementById('timer');
 let quizzScore = 0
 let setTime;
+let y;
 
 
 
@@ -23,7 +24,7 @@ const quizzStart = () => {
     start.style.display ="none"
     question.style.display = 'block'
 
-    timex(40);
+    timex(60);
     
 }
 
@@ -36,6 +37,7 @@ let timex = function(x) {
     }
 
     timeElm.innerHTML = x;
+    y = x
     setTime = setTimeout(() => {timex(--x)}, 1000)
 
     return setTime;
@@ -50,12 +52,14 @@ const seeAnswer = (event) => {
     let quest1 = event.target.dataset['number']
     if(quest1 == 4) {
         quizzScore= quizzScore + 25
-        console.log(quizzScore)
         answerTag.textContent = 'correct'
         setTimeout(() => {answerTag.textContent=""}, 300);
     } else {
         answerTag.textContent = 'wrong'
         setTimeout(() => {answerTag.textContent=""}, 300);
+        stopFunction()
+        y = y-10
+        timex(y)
     }
     question.style.display = 'none'
     question1.style.display = 'block'
@@ -67,12 +71,14 @@ const seeAnswer1 = (event) => {
     let quest2 = event.target.dataset['number']
     if(quest2 == 3) {
         quizzScore = quizzScore + 25
-        console.log(quizzScore)
         answerTag.textContent = 'correct'
         setTimeout(() => {answerTag.textContent=""}, 300);
     } else {
         answerTag.textContent = 'wrong'
         setTimeout(() => {answerTag.textContent=""}, 300);
+        stopFunction()
+        y = y-10
+        timex(y)
     }
     question1.style.display = 'none'
     question2.style.display = 'block'
@@ -84,12 +90,14 @@ const seeAnswer2 = (event) => {
     let quest3 = event.target.dataset['number']
     if(quest3 == 1) {
         quizzScore = quizzScore + 25
-        console.log(quizzScore)
         answerTag.textContent = 'correct'
         setTimeout(() => {answerTag.textContent=""}, 300);
     } else {
         answerTag.textContent = 'wrong'
         setTimeout(() => {answerTag.textContent=""}, 300);
+        stopFunction()
+        y = y-10
+        timex(y)
     }
     question2.style.display = 'none'
     question3.style.display = 'block'
@@ -100,12 +108,14 @@ const seeAnswer3 = (event) => {
     let quest4 = event.target.dataset['number']
     if(quest4 == 4) {
         quizzScore = quizzScore + 25
-        console.log(quizzScore)
         answerTag.textContent = 'correct'
         setTimeout(() => {answerTag.textContent=""}, 300);
     } else {
         answerTag.textContent = 'wrong'
         setTimeout(() => {answerTag.textContent=""}, 300);
+        stopFunction()
+        y = y-10
+        timex(y)
     }
     question3.style.display = 'none';
     allDone.style.display = 'block';
